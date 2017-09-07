43d82922c889c0f19e852f270b220e4a39e27084
Shermine Jong <shermine_jong_ruth_93@outlook.sg>
2017-09-07 13:51:37 +0800

    Implement Basic function for using statements and statement List

--- EmptyGeneralTesting/source/PKB/Statement.cpp
+++ EmptyGeneralTesting/source/PKB/Statement.cpp
@@ -1,14 +1,51 @@
 #include "Statement.h"
 
 Statement::Statement() {
-
+	stmtNo = NULL;
+	parentStatement = nullptr;
+	//parentProcedure = nullptr;
+	follow = nullptr;
 }
 
-Statement::Statement(int i, string content) {
-	index = i;
-	contentStatement = content;
+Statement::Statement(int index, Statement *parent, Statement *following) {
+	stmtNo = index;
+	parentStatement = parent;
+	follow = following;
+	followBy = nullptr;
 }
 
-void Statement::setStmtLst(StatementList &stmtList) {
-	stmtLst = &stmtList;
+/*Statement::Statement(int index, Procedure parent, Statement *following) {
+stmtNo = index;
+parentProcedure = parent;
+follow = following;
+}*/
+
+void Statement::setFollowedBy(Statement *followedBy) {
+	followBy = followedBy;
+}
+Statement* Statement::getFollow() {
+	return follow;
+}
+bool Statement::isFollow(int stmtNo) {
+	return follow != nullptr && follow->stmtNo == stmtNo;
+}
+Statement* Statement::getFollowBy() {
+	return followBy;
+}
+bool Statement::isFollowBy(int stmtNo) {
+	return followBy != nullptr && followBy->stmtNo == stmtNo;
+}
+Statement* Statement::getParentStatement() {
+	return parentStatement;
+}
+//Procedure getParentProcedure();
+bool Statement::isParent(int stmtNo) {
+	return parentStatement != nullptr && parentStatement->stmtNo == stmtNo;
+}
+//bool isParent(string proName);
+int Statement::getStmtNo() {
+	return stmtNo;
+}
+bool Statement::equals(Statement* others) {
+	return stmtNo == others->stmtNo;
 }
